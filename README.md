f885f7087dc4efa919ead38008877e60fd094848
Joe Hudson <joehud@gmail.com>
2015-03-11 07:52:32 -0400

    updates

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # golang-chat-example
 Simple example to help me learn golang
+
+A chat client (TODO) is intended to be used with the server, but a standard telnet connection can be used
+
+Right now, the server will run on port ```5000``` but it will eventually accept port details from a config file.
+
+To run the server
+
+```
+> git clone https://github.com/jhudson8/golang-chat-example.git
+> cd golang-chat-example
+> go 
+```
+
+And, until the client has been created, in another terminal window
+
+```
+> telnet localhost 5000
+> /user joe
+> /chat hello
+> /quit
+```
---
+++ server.go
@@ -0,0 +1,119 @@
+
+// Simple chat server which uses connection properties from "connection.json" in same directory
+// Simple chat client is intended to be used but a standard telnet connection can be used
+// > telnet {host} {port}
+// > /user {username}
+// > /chat {message}
+//
+// reference: https://parroty00.wordpress.com/2013/07/18/golang-tcp-server-example/
+package main
+
+import (
+  "net"
+  "fmt"
+  "bufio"
+  "strings"
+  "regexp"
+)
+
+// Container for client username and connection details
+type Client struct {
+  Connection net.Conn
+  Username string
+}
+
+// Close the client connection and clenup
+func (c Client) Close() {
+  c.Connection.Close();
+}
+
+// static client list
+var availableClients [256]Client
+var clients []Client
+
+// program main
+func main() {
+  // start the server
+  psock, err := net.Listen("tcp", ":5000")
+  if err != nil { return }
+ 
+  for {
+    // accept connections
+    conn, err := psock.Accept()
+    if err != nil { return }
+
+    // keep track of the client details
+    client := Client{Connection: conn}
+    addClient(client);
+
+    // allow non-blocking client request handling
+    channel := make(chan string)
+    go waitForInput(channel, client)
+    go handleInput(channel, client)
+  }
+}
+
+// wait for client input (buffered by newlines) and signal the channel
+func waitForInput(out chan string, client Client) {
+  defer close(out)
+ 
+  for {
+    line, err := bufio.NewReader(client.Connection).ReadBytes('\n')
+    if err != nil {
+      // connection has been closed, remove the client
+      client.Close();
+      return
+    }
+    out <- string(line)
+  }
+}
+
+// listen for channel updates for a client and handle the message
+// messages must be in the format of /{action} {content} where content is optional depending on the action
+// supported actions are "user", "chat", and "quit".  the "user" must be set before any chat messages are allowed
+func handleInput(in <-chan string, client Client) {
+  for {
+    message := <- in
+    message = strings.TrimSpace(message)
+    action, rest := getAction(message)
+
+    if (action != "") {
+      switch action {
+        case "chat":
+          sendMessage(rest, client)
+        case "user":
+          client.Username = rest
+        case "quit":
+          client.Close();
+      }
+    }
+  }
+}
+
+// sent a message to all clients (except the sender)
+func sendMessage(message string, client Client) {
+  message = fmt.Sprintf("[%v] %v\n", client.Username, message);
+
+  for _, client := range clients {
+    // write the message to the client
+    fmt.Fprintf(client.Connection, message)
+  }
+}
+
+// parse out message contents (/{action} {message}) and return individual values
+func getAction(message string) (string, string) {
+  actionRegex, _ := regexp.Compile(`^\/([^\s]*)\s*(.*)$`)
+  res := actionRegex.FindAllStringSubmatch(message, -1)
+  if (len(res) == 1) {
+    return res[0][1], res[0][2]
+  }
+  return "", ""
+}
+
+// cache a new client
+func addClient(value Client) {
+  fmt.Printf("Adding client %v\n", value)
+  numClients := len(clients)
+  availableClients[numClients] = value;
+  clients = availableClients[0:numClients+1]
+}
